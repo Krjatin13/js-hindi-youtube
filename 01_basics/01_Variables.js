84076164ff6cae6b7a75c311f6388540bdc0da79
KUMAR JATIN <kj968000@gmail.com>
2025-07-14 15:55:09 +0000

    datatypes summary

--- 01_basics/01_Variables.js
+++ 01_basics/01_Variables.js
@@ -1,1 +1,19 @@
+const accountId = 144553
+let accountEmail = "kj968000@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
 
+
+accountEmail = "hc@hc.com"
+accountPassword = "21212154"
+accountCity = "Bengaluru"
+
+
+console.log(accountId);
+
+/*
+Prefer not to use var 
+because of issue in block scope and functional scope
+*/
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
