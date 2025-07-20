9af6ba1a2f7a1d0fd80010947e696ff9cc74d61f
KUMAR JATIN <kj968000@gmail.com>
2025-07-20 14:24:45 +0000

    json

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,32 @@
+// singleton
+// Object.create
+
+// object literals
+const JsUser = {
+    name : "Hitesh",
+    age :  18,
+    location: "Jaipur",
+    email: "hitesh@google.com",
+    isLoggedIn: false,
+    lastLoginDays: ["Monday", "Saturday"]
+}
+
+console.log(JsUser.email)
+console.log(JsUser["email"])
+
+
+JsUser.email = "hitesh@chatgpt.com"
+Object.freeze(JsUser)
+JsUser.email = "hitesh@microsoft.com"
+console.log(JsUser);
+
+JsUser.greeting = function(){
+    console.log("Helllo JS user");
+}
+
+JsUser.greetingTwo = function(){
+    console.log(`Hello JS user, ${this.name}`);
+}
+
+console.log(JsUser.greeting());
+console.log(JsUser.greetingTwo());
